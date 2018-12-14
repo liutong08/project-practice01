035a6dd014c5a092a8e695c4903851aef583986d
liutong08 <939762677@qq.com>
2018-12-14 12:43:33 +0800

    Initial commit

---
+++ src/test/java/cn/com/taiji/ProjectPractice01ApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.com.taiji;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectPractice01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
