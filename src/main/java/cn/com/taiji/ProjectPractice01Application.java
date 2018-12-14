526f4b695233297bb0e2de2a0768b2966f7c974e
zhengwei <613295775@qq.com>
2018-12-14 13:24:04 +0800

    更新

--- src/main/java/cn/com/taiji/ProjectPractice01Application.java
+++ src/main/java/cn/com/taiji/ProjectPractice01Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectPractice01Application {
 
     public static void main(String[] args) {
-        System.out.println("222222");
+
         
         System.out.println();
         SpringApplication.run(ProjectPractice01Application.class, args);
