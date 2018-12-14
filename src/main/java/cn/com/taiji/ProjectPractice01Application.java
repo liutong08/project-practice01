2ba8de41ea193effc100278e54e36d4eab31ecfd
马玉龙 <1187244022@qq.com>
2018-12-14 13:21:58 +0800

    dd

--- src/main/java/cn/com/taiji/ProjectPractice01Application.java
+++ src/main/java/cn/com/taiji/ProjectPractice01Application.java
@@ -8,6 +8,8 @@ public class ProjectPractice01Application {
 
     public static void main(String[] args) {
         System.out.println("222222");
+        
+        System.out.println();
         SpringApplication.run(ProjectPractice01Application.class, args);
         
         
